803c4563e026fa28288a78a3577dbd6074d82f7c
redrumkumg <n.ronnapat@github.com>
2024-05-15 13:30:14 +0700

    apply

---
+++ index.js
@@ -0,0 +1,17 @@
+let myAlphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+const myAlphabetLength = () => {
+  console.log(myAlphabet.length);
+};
+
+myAlphabetLength();
+
+const Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
+Planets.forEach(function (members) {
+  console.log(members);
+});
+
+const myArr = [1, 2, "One", true];
+myArr.forEach(function (members) {
+  console.log(members);
+});
