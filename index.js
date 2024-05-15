0cf043d1295906a75657f1ca70ef890aba94090c
redrumkumg <n.ronnapat@github.com>
2024-05-15 13:42:18 +0700

    fixed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let myAlphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
-const myAlphabetLength = () => {
-  console.log(myAlphabet.length);
+const myAlphabetLength = (array) => {
+  console.log(array.length);
 };
 
-myAlphabetLength();
+myAlphabetLength(myAlphabet);
 
 const Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
-Planets.forEach(function (members) {
-  console.log(members);
+Planets.forEach(function (members, index, test) {
+  console.log(members, index, test);
 });
 
 const myArr = [1, 2, "One", true];
-myArr.forEach(function (members) {
-  console.log(members);
+myArr.forEach(function (members, index) {
+  console.log(members, index);
 });
